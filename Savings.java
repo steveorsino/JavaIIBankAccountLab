25c80f8af8181427a42ac4433e63d8a01425fa9a
steveorsino <steveorsino@yahoo.com>
2017-03-20 10:26:35 -0400

    Buit out Savings subclass

--- Savings.java
+++ Savings.java
@@ -1,4 +1,30 @@
 
-public class Savings {
-
+public class Savings extends BankAccount {
+	
+	private double interestRate;
+	
+	public Savings() {
+		super();
+		interestRate = 0;
+	}
+	
+	public void SetInterestRate(double ir) {
+		interestRate = ir;
+	}
+	
+	
+	public double GetInterestRate() {
+		return interestRate;
+	}
+	
+	
+	public double MonthlyFees() {
+		return 5.0;
+	}
+	
+	public void DisplayFees() {
+		System.out.println("Account Number: " + "\t" + this.GetAccountNumber() + "\n" +
+				"Interest Rate: " + this.GetInterestRate() + "\n" +
+				"Monthly Fees: " + this.MonthlyFees());
+	}
 }
